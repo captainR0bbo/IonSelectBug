7835e73c7a61cf77c14f7c734869bb8b84491540
Rob Lipstreu <rlipstreu@tranzonic.com>
2021-10-06 13:02:44 -0400

    Ionic beta and repro code

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,53 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { Redirect, Route } from "react-router-dom";
+import {
+  IonApp,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonRouterOutlet,
+  IonSelect,
+  IonSelectOption,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import Home from "./pages/Home";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <IonContent>
+      <IonItem>
+        <IonLabel position="floating">Select test</IonLabel>
+        <IonSelect>
+          <IonSelectOption key={1} value={1}>
+            1
+          </IonSelectOption>
+          <IonSelectOption key={2} value={2}>
+            2
+          </IonSelectOption>
+          <IonSelectOption key={3} value={3}>
+            3
+          </IonSelectOption>
+        </IonSelect>
+      </IonItem>
+    </IonContent>
   </IonApp>
 );
 
